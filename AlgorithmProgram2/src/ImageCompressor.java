b83f449887f82828ab572040277634f063bd9a9a
j4k13 <namlesscrusader@gmail.com>
2016-03-20 22:13:23 -0400

    start imagecompress problem

---
+++ AlgorithmProgram2/src/ImageCompressor.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Created by jackie on 3/20/16.
+ */
+public class ImageCompressor {
+    //private
+    public void compress(File image)
+    {
+        //read line give
+        //tally each column in this arraylist
+        ArrayList<Integer> columnTally = new ArrayList<Integer>();
+
+    }
+
+    public int findDisruption()
+    {
+        return 0;
+    }
+
+    public static void main(String [] args)
+    {
+        //give file
+    }
+}
