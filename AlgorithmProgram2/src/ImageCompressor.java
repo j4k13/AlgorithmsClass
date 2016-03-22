b9612d7a70fe24c72cb1ee3f1d22759556a71000
j4k13 <namlesscrusader@gmail.com>
2016-03-22 00:27:30 -0400

    started basic solution

--- AlgorithmProgram2/src/ImageCompressor.java
+++ AlgorithmProgram2/src/ImageCompressor.java
@@ -1,5 +1,8 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 /**
  * Created by jackie on 3/20/16.
@@ -8,19 +11,36 @@ public class ImageCompressor {
     //private
     public void compress(File image)
     {
-        //read line give
-        //tally each column in this arraylist
+        //some preliminary code to make set the length of the array list
         ArrayList<Integer> columnTally = new ArrayList<Integer>();
+        String nextline = "";
+        int index = 0;
+        try
+        {
+            Scanner in = new Scanner(image);
+            while(in.hasNextLine())
+            {
+                //read line give
+                nextline = in.nextLine();
+                //break the line down
+                String [] nextsetofvalues = nextline.split(" ");
+                //tally each column
+                columnTally.set(index, (Integer.parseInt(nextsetofvalues[index] + columnTally.get(index))));
 
-    }
+            }
+            in.close();
+            int leastdisruptive = Collections.min(columnTally);
+        }
+        catch(IOException e)
+        {
 
-    public int findDisruption()
-    {
-        return 0;
+        }
+
     }
 
     public static void main(String [] args)
     {
         //give file
+
     }
 }
