ff2456ed61784d1b34fc65f6fbfa474babf5b831
LANDON <landon.reams.13@cnu.edu>
2016-04-28 05:11:58 -0400

    Final refinements to make variables easier to change.

--- LastAssignment/src/LastAssignment.java
+++ LastAssignment/src/LastAssignment.java
@@ -1,18 +1,46 @@
 import java.util.Arrays;
 
-import LastAssignment.Alg;
-
 /**
  * Created by jackie on 4/26/16.
  */
 public class LastAssignment {
 
 	/**
+	 * The maximum size that can be stored in a bin.
+	 */
+	public static final double MAX_BIN_SIZE = 1;
+
+	/**
+	 * The number of data points to simulate.
+	 */
+	public static final int DATA_COUNT = 100;
+
+	/**
+	 * The number of trials to simulate.
+	 */
+	public static final int TRIAL_COUNT = 100;
+
+	/**
+	 * The interval at which trials will be printed to the console.
+	 */
+	public static final int TRIAL_LOGGING_INTERVAL = 50;
+
+	/**
+	 * The maximum value for a data point.
+	 */
+	public static final double DATA_MAX = 1.0;
+
+	/**
+	 * The minimum value for a data point.
+	 */
+	public static final double DATA_MIN = 0.0;
+
+	/**
 	 * Returns the data packed into bins using a Sorted First Fit Algorithm.
 	 * The element at index 0 is the # of Bins Used.
 	 * The rest are the resulting packed bins.
-	 * @param data
-	 * @return bins
+	 * @param data Double array of data. Assumed to be between {@link #DATA_MIN} and {@link #DATA_MAX}
+	 * @return Double array of data packed into bins.
 	 */
 	public static double[] alg_sorted_first_fit(double[] data) {
 		double[] bins = new double[data.length + 1];
@@ -22,7 +50,7 @@ public class LastAssignment {
 		//Heh, double d
 		for(double d : data) {
 			binLoc = 1;
-			while(bins[binLoc] + d > 1) {
+			while(bins[binLoc] + d > MAX_BIN_SIZE) {
 				binLoc++;
 			}
 			bins[binLoc] += d;
@@ -38,8 +66,8 @@ public class LastAssignment {
 	 * Returns the data packed into bins using a Best Fit algorithm.
 	 * The element at index 0 is the # of Bins Used.
 	 * The rest are the resulting packed bins.
-	 * @param data
-	 * @return bins
+	 * @param data Double array of data. Assumed to be between {@link #DATA_MIN} and {@link #DATA_MAX}
+	 * @return Double array of data packed into bins.
 	 */
 	public static double[] alg_best_fit(double[] data) {
 		double[] bins = new double[data.length + 1];
@@ -58,7 +86,7 @@ public class LastAssignment {
 
 			while(binLoc < (binsUsed + 1)) {
 				sum = d + bins[binLoc];
-				if(sum < 1 && sum > bestBinValue) {
+				if(sum < MAX_BIN_SIZE && sum > bestBinValue) {
 					//A best fit has been found! Remember it.
 					bestBin = binLoc;
 					bestBinValue = sum;
@@ -80,23 +108,23 @@ public class LastAssignment {
 		return bins;
 	}
 
+	/**
+	 * Populates a data set with values between {@link #DATA_MIN} and {@link #DATA_MAX}
+	 * @param arr Array of doubles
+	 */
 	public static void populate(double[] arr) {
 		for(int i = 0; i < arr.length; i++) {
-			arr[i] = Math.random();
+			arr[i] = DATA_MIN + (DATA_MAX - DATA_MIN) * Math.random();
 		}
 	}
 
 	private enum Alg { A, B; }
 
 	public static void main(String[] args) {
-		int size = 100;
-		int totalTrials = 1000;
-		int trialInterval = 10;
+		Alg[] winnersTime = new Alg[TRIAL_COUNT];
+		Alg[] winnersBins = new Alg[TRIAL_COUNT];
 
-		Alg[] winnersTime = new Alg[totalTrials];
-		Alg[] winnersBins = new Alg[totalTrials];
-
-		double[] rawData = new double[size];
+		double[] rawData = new double[DATA_COUNT];
 		double[] data_1, data_2, bins_1 = null, bins_2 = null;
 
 		long startTime_1, endTime_1, startTime_2, endTime_2, diff_1, diff_2;
@@ -107,7 +135,7 @@ public class LastAssignment {
 
 		startTime_2 = 0L; endTime_2 = 0L;
 
-		for(int trial = 1; trial < totalTrials+1; trial++) {
+		for(int trial = 1; trial < TRIAL_COUNT+1; trial++) {
 			populate(rawData);
 
 			data_1 = rawData.clone();
@@ -138,18 +166,18 @@ public class LastAssignment {
 			avgBinsA += bins_1[0];
 			avgBinsB += bins_2[0];
 
-			if(trial % trialInterval == 0) {
-				System.out.printf("TRIAL %" + (int)(Math.log10(totalTrials)+1) +
+			if(trial % TRIAL_LOGGING_INTERVAL == 0) {
+				System.out.printf("TRIAL %" + (int)(Math.log10(TRIAL_COUNT)+1) +
 					"d -- ALG1: %,7dns | ALG2: %,7dns%n",
 					trial, endTime_1 - startTime_1,
 					       endTime_2 - startTime_2);
 			}
 		}
 
-		avgTimeA /= totalTrials;
-		avgTimeB /= totalTrials;
-		avgBinsA /= totalTrials;
-		avgBinsB /= totalTrials;
+		avgTimeA /= TRIAL_COUNT;
+		avgTimeB /= TRIAL_COUNT;
+		avgBinsA /= TRIAL_COUNT;
+		avgBinsB /= TRIAL_COUNT;
 
 		int aTimeSum = 0, bTimeSum = 0;
 
@@ -165,11 +193,20 @@ public class LastAssignment {
 			else           bBinsSum++;
 		}
 
-		double timePercent = (double)(Math.max(aTimeSum, bTimeSum)) / totalTrials;
-		double binsPercent = (double)(Math.max(aBinsSum, bBinsSum)) / totalTrials;
+		double timePercent = (double)(Math.max(aTimeSum, bTimeSum)) / TRIAL_COUNT;
+		double binsPercent = (double)(Math.max(aBinsSum, bBinsSum)) / TRIAL_COUNT;
 
-		System.out.printf("RESULTS!%nRUNTIME:  %s won %3d%% of the trials! (%d to %d)%n", aTimeSum > bTimeSum ? "A" : "B", (int)(100 * timePercent), aTimeSum, bTimeSum);
-		System.out.printf("BINCOUNT: %s won %3d%% of the trials! (%d to %d)%n", aBinsSum > bBinsSum ? "A" : "B", (int)(100 * binsPercent), aBinsSum, bBinsSum);
+		System.out.println("RESULTS!");
+
+		if(aTimeSum == bTimeSum)
+			System.out.println("RUNTIME:  There was a tie!");
+		else
+			System.out.printf("RUNTIME:  %s won %3d%% of the trials! (%d to %d)%n", aTimeSum > bTimeSum ? "A" : "B", (int)(100 * timePercent), aTimeSum, bTimeSum);
+
+		if(aBinsSum == bBinsSum)
+			System.out.println("BINCOUNT: There was a tie!");
+		else
+			System.out.printf("BINCOUNT: %s won %3d%% of the trials! (%d to %d)%n", aBinsSum > bBinsSum ? "A" : "B", (int)(100 * binsPercent), aBinsSum, bBinsSum);
 		System.out.printf("On average, Algorithm A took %,6dns and produced %2d bins.%n", (int)avgTimeA, (int)avgBinsA);
 		System.out.printf("On average, Algorithm B took %,6dns and produced %2d bins.%n", (int)avgTimeB, (int)avgBinsB);
 	}
